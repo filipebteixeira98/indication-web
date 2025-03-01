04ebcbb0bdbdb70904ea94deda5b8b7423afd4c3
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-02-28 21:55:29 -0300

    refactor(invite): page to create ranking component

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -2,12 +2,11 @@ import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import Image from 'next/image'
 
 import logo from '@/assets/logo.svg'
-import medalCooper from '@/assets/medal-cooper.svg'
-import medalGold from '@/assets/medal-gold.svg'
-import medalSilver from '@/assets/medal-silver.svg'
 
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
+
+import { Ranking } from './ranking'
 
 export default function InvitePage() {
   return (
@@ -75,48 +74,7 @@ export default function InvitePage() {
           </div>
         </div>
       </div>
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Nomination ranking
-        </h2>
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1st</span> | John Doe
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image src={medalGold} alt="" className="absolute top-0 right-8" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2nd</span> | John Doe
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={medalSilver}
-              alt=""
-              className="absolute top-0 right-8"
-            />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3rd</span> | John Doe
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={medalCooper}
-              alt=""
-              className="absolute top-0 right-8"
-            />
-          </div>
-        </div>
-      </div>
+      <Ranking />
     </div>
   )
 }
