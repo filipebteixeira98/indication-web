617b6502153e79c2ac9a19bbf60dadb37400b2cd
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-02-28 21:52:49 -0300

    chore: create invite page structure

---
+++ src/app/invite/page.tsx
@@ -0,0 +1,122 @@
+import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
+import Image from 'next/image'
+
+import logo from '@/assets/logo.svg'
+import medalCooper from '@/assets/medal-cooper.svg'
+import medalGold from '@/assets/medal-gold.svg'
+import medalSilver from '@/assets/medal-silver.svg'
+
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+
+export default function InvitePage() {
+  return (
+    <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
+      <div className="flex flex-col gap-10 w-full max-w-[550px]">
+        <Image src={logo} alt="devstage" width={108.5} height={30} />
+        <div className="space-y-2">
+          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+            Registration confirmed!
+          </h1>
+          <p className="text-gray-300">
+            To enter the event, access the link sent to your email.
+          </p>
+        </div>
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+              Refer and earn
+            </h2>
+            <p className="text-gray-300">
+              Invite more people to the event and compete for prizes exclusive!
+              Just share the link below and follow the registrations:
+            </p>
+          </div>
+          <InputRoot>
+            <InputIcon>
+              <Link className="size-5" />
+            </InputIcon>
+            <InputField
+              readOnly
+              defaultValue="http://localhost:3333/invite/ry38sa879fas87g7f8a"
+            />
+            <IconButton className="-mr-2">
+              <Copy className="size-5" />
+            </IconButton>
+          </InputRoot>
+          <div className="grid gap-3 md:grid-cols-3">
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                1042
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Access to the link
+              </span>
+              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                875
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Registrations made
+              </span>
+              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                3rd
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Position in the ranking
+              </span>
+              <Medal className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Nomination ranking
+        </h2>
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1st</span> | John Doe
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image src={medalGold} alt="" className="absolute top-0 right-8" />
+          </div>
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2nd</span> | John Doe
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image
+              src={medalSilver}
+              alt=""
+              className="absolute top-0 right-8"
+            />
+          </div>
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3rd</span> | John Doe
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image
+              src={medalCooper}
+              alt=""
+              className="absolute top-0 right-8"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
