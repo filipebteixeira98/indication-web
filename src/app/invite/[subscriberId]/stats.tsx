84837a011fd7986aa0665642290ca7b70c0ad211
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-05-09 10:23:49 -0300

    feat(stats): implement ranking and engagement metrics display

--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,11 +1,29 @@
+import {
+  getSubscribersSubscriberIdRankingClicks,
+  getSubscribersSubscriberIdRankingCount,
+  getSubscribersSubscriberIdRankingPosition,
+} from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
 
-export function Stats() {
+interface StatsProps {
+  subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } =
+    await getSubscribersSubscriberIdRankingClicks(subscriberId)
+
+  const { count: inviteCount } =
+    await getSubscribersSubscriberIdRankingCount(subscriberId)
+
+  const { position: rankingPosition } =
+    await getSubscribersSubscriberIdRankingPosition(subscriberId)
+
   return (
     <div className="grid gap-3 md:grid-cols-3">
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {accessCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Access to the link
@@ -14,7 +32,7 @@ export function Stats() {
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          875
+          {inviteCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Registrations made
@@ -23,7 +41,7 @@ export function Stats() {
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          3rd
+          {rankingPosition ? `${rankingPosition}º` : '-'}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Position in the ranking
